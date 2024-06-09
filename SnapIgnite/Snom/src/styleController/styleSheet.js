730b2fc1b8585768cfb1e3890d0e9e67edb9bc0f
rahat.adnan <sthep90@Gmail.com>
2024-06-10 00:57:57 +0600

    added snap element in snap stylesheet

--- SnapIgnite/Snom/src/styleController/styleSheet.js
+++ SnapIgnite/Snom/src/styleController/styleSheet.js
@@ -5,7 +5,6 @@ export default class StyleSheet{
     constructor(snapElement) {
         if(!snapElement instanceof Snom) throw new Error('Invalid Snom Element Found.');
         this.creatorElement = snapElement
-        this.inline_styles = dom_helper.parseStyle();
-
+        this.inline_styles = dom_helper.parseStyle(snapElement.snom_element);
     }
 }
